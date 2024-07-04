a561b497425d47223884824ed1f5af88f5870906
EasyCode <ezekielayodele1@gmail.com>
2024-07-04 17:03:53 +0100

    Added loadThemeState method to ThemeManager class and refactored initialization
    - Added loadThemeState method to ThemeManager class
    - Refactored code by creating a function to initialize Thememanager
    - Created a function to initialize UiManager

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,7 @@
 "use strict";
+const THEME_LIGHT = 'light';
+const THEME_DARK = 'dark';
+const HIDE_CLASS = 'hide';
 class UiManager {
     constructor(optionsMenu) {
         this.isMenuOptionOpen = false;
@@ -7,30 +10,46 @@ class UiManager {
     toggleOptionMenu() {
         this.isMenuOptionOpen = !this.isMenuOptionOpen;
         if (this.isMenuOptionOpen) {
-            this.optionsMenu.classList.remove('hide');
+            this.optionsMenu.classList.remove(HIDE_CLASS);
             return 'Menu options opened';
         }
         else {
-            this.optionsMenu.classList.add('hide');
+            this.optionsMenu.classList.add(HIDE_CLASS);
             return 'Menu options closed';
         }
     }
 }
 class ThemeManager {
     constructor(appWrapperElem) {
+        this.themeMode = '';
         this._appWrapper = appWrapperElem;
+        this.loadThemeState();
+    }
+    loadThemeState() {
+        const storedTheme = localStorage.getItem('theme');
+        this.themeMode = storedTheme ? JSON.parse(storedTheme) : THEME_LIGHT;
+        this.applyTheme(this.themeMode);
+        return `${this.themeMode} theme`;
+    }
+    applyTheme(theme) {
+        theme === THEME_LIGHT ? this.activateLightMode() : this.activateDarkMode();
     }
     activateLightMode() {
         this._appWrapper.classList.remove('dark__mode');
-        this._appWrapper.classList.add('light__mode');
+        this.themeMode = THEME_LIGHT;
+        localStorage.setItem('theme', JSON.stringify(this.themeMode));
         console.log('Light mode activated');
         return 'Light mode activated';
     }
     activateDarkMode() {
-        this._appWrapper.classList.remove('light__mode');
         this._appWrapper.classList.add('dark__mode');
+        this.themeMode = THEME_DARK;
+        localStorage.setItem('theme', JSON.stringify(this.themeMode));
         console.log('Dark mode activated');
         return 'Dark mode activated';
+    }
+    getThemeMode() {
+        return this.themeMode;
     }
 }
 class ListManager {
@@ -65,18 +84,21 @@ const lightModeButton = document.querySelector('.light__switch');
 const darkModeButton = document.querySelector('.dark__switch');
 const optionsMenu = document.querySelector('.options__menu');
 const optionsMenuIcon = document.querySelector('.three__dots');
-if (optionsMenu && optionsMenuIcon) {
+const checkElement = (element, desc) => {
+    if (!element) {
+        throw new Error(`${desc} not found`);
+    }
+};
+const initializeUiManager = (optionsMenu, optionsMenuIcon) => {
+    checkElement(optionsMenu, 'Options Menu');
+    checkElement(optionsMenuIcon, 'Options Menu');
     const uiManager = new UiManager(optionsMenu);
     optionsMenuIcon.addEventListener('click', () => uiManager.toggleOptionMenu());
-}
-else {
-    console.log('Option Menu not or optionsMenuIcon not found');
-}
-if (lightModeButton && darkModeButton && uiAppElement) {
-    const themeManager = new ThemeManager(uiAppElement);
+};
+const initializeLightAndDarkModeButtons = (themeManager, lightModeButton, darkModeButton) => {
     const toggleLightAndDarkButton = () => {
-        lightModeButton.classList.toggle('hide');
-        darkModeButton.classList.toggle('hide');
+        lightModeButton.classList.toggle(HIDE_CLASS);
+        darkModeButton.classList.toggle(HIDE_CLASS);
     };
     lightModeButton.addEventListener('click', () => {
         themeManager.activateLightMode();
@@ -86,7 +108,24 @@ if (lightModeButton && darkModeButton && uiAppElement) {
         themeManager.activateDarkMode();
         toggleLightAndDarkButton();
     });
-}
-else {
-    console.warn('Light mode button or Dark Mode button not found');
-}
+};
+const setInitialButtonState = (themeManager, lightModeButton, darkModeButton) => {
+    if (themeManager.getThemeMode() === THEME_LIGHT) {
+        lightModeButton.classList.add(HIDE_CLASS);
+        darkModeButton === null || darkModeButton === void 0 ? void 0 : darkModeButton.classList.remove(HIDE_CLASS);
+    }
+    else {
+        darkModeButton === null || darkModeButton === void 0 ? void 0 : darkModeButton.classList.add(HIDE_CLASS);
+        lightModeButton === null || lightModeButton === void 0 ? void 0 : lightModeButton.classList.remove(HIDE_CLASS);
+    }
+};
+const initializeThemeManager = (uiAppElement, lightModeButton, darkModeButton) => {
+    checkElement(uiAppElement, 'Ui app Element');
+    checkElement(lightModeButton, 'light mode button');
+    checkElement(darkModeButton, 'dark mode button');
+    const themeManager = new ThemeManager(uiAppElement);
+    setInitialButtonState(themeManager, lightModeButton, darkModeButton);
+    initializeLightAndDarkModeButtons(themeManager, lightModeButton, darkModeButton);
+};
+initializeThemeManager(uiAppElement, lightModeButton, darkModeButton);
+initializeUiManager(optionsMenu, optionsMenuIcon);
--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,19 @@ interface ListManagerInterface {
 
 type UiElement = HTMLUListElement | HTMLDivElement
 
+const THEME_LIGHT = 'light'
+const THEME_DARK = 'dark'
+const HIDE_CLASS = 'hide'
+
 interface UiManagerInterface {
   toggleOptionMenu(): string
 }
 
 interface ThemeManagerInterface {
+  loadThemeState(): string
   activateLightMode(): string
   activateDarkMode(): string
+  getThemeMode(): string
 }
 
 class UiManager implements UiManagerInterface {
@@ -34,10 +40,10 @@ class UiManager implements UiManagerInterface {
     this.isMenuOptionOpen = !this.isMenuOptionOpen
 
     if (this.isMenuOptionOpen) {
-      this.optionsMenu.classList.remove('hide')
+      this.optionsMenu.classList.remove(HIDE_CLASS)
       return 'Menu options opened'
     } else {
-      this.optionsMenu.classList.add('hide')
+      this.optionsMenu.classList.add(HIDE_CLASS)
       return 'Menu options closed'
     }
   }
@@ -45,24 +51,42 @@ class UiManager implements UiManagerInterface {
 
 class ThemeManager implements ThemeManagerInterface {
   private _appWrapper: UiElement
+  private themeMode: string = ''
 
   constructor(appWrapperElem: UiElement) {
     this._appWrapper = appWrapperElem
+    this.loadThemeState()
+  }
+
+  loadThemeState(): string {
+    const storedTheme = localStorage.getItem('theme')
+    this.themeMode = storedTheme ? JSON.parse(storedTheme) : THEME_LIGHT
+    this.applyTheme(this.themeMode)
+    return `${this.themeMode} theme`
+  }
+
+  applyTheme(theme: string): void {
+    theme === THEME_LIGHT ? this.activateLightMode() : this.activateDarkMode()
   }
 
   activateLightMode(): string {
     this._appWrapper.classList.remove('dark__mode')
-    this._appWrapper.classList.add('light__mode')
+    this.themeMode = THEME_LIGHT
+    localStorage.setItem('theme', JSON.stringify(this.themeMode))
     console.log('Light mode activated')
     return 'Light mode activated'
   }
 
   activateDarkMode(): string {
-    this._appWrapper.classList.remove('light__mode')
     this._appWrapper.classList.add('dark__mode')
+    this.themeMode = THEME_DARK
+    localStorage.setItem('theme', JSON.stringify(this.themeMode))
     console.log('Dark mode activated')
-
     return 'Dark mode activated'
+  }
+
+  getThemeMode(): string {
+    return this.themeMode
   }
 }
 
@@ -97,36 +121,42 @@ class ListManager implements ListManagerInterface {
 const uiListElement = document.querySelector(
   '.item__container'
 ) as HTMLUListElement | null
-const uiAppElement = document.querySelector('.app') as HTMLDivElement | null
+const uiAppElement = document.querySelector('.app') as HTMLDivElement
 
 const lightModeButton = document.querySelector(
   '.light__switch'
-) as HTMLDivElement | null
-const darkModeButton = document.querySelector(
-  '.dark__switch'
-) as HTMLDivElement | null
+) as HTMLDivElement
+const darkModeButton = document.querySelector('.dark__switch') as HTMLDivElement
 
-const optionsMenu = document.querySelector(
-  '.options__menu'
-) as HTMLDivElement | null
+const optionsMenu = document.querySelector('.options__menu') as HTMLDivElement
 
-const optionsMenuIcon = document.querySelector(
-  '.three__dots'
-) as HTMLDivElement | null
+const optionsMenuIcon = document.querySelector('.three__dots') as HTMLDivElement
 
-if (optionsMenu && optionsMenuIcon) {
-  const uiManager = new UiManager(optionsMenu)
-  optionsMenuIcon.addEventListener('click', () => uiManager.toggleOptionMenu())
-} else {
-  console.log('Option Menu not or optionsMenuIcon not found')
+const checkElement = (element: UiElement, desc: string): void => {
+  if (!element) {
+    throw new Error(`${desc} not found`)
+  }
 }
 
-if (lightModeButton && darkModeButton && uiAppElement) {
-  const themeManager = new ThemeManager(uiAppElement)
+const initializeUiManager = (
+  optionsMenu: HTMLDivElement,
+  optionsMenuIcon: HTMLDivElement
+) => {
+  checkElement(optionsMenu, 'Options Menu')
+  checkElement(optionsMenuIcon, 'Options Menu')
 
-  const toggleLightAndDarkButton = () => {
-    lightModeButton.classList.toggle('hide')
-    darkModeButton.classList.toggle('hide')
+  const uiManager = new UiManager(optionsMenu)
+  optionsMenuIcon.addEventListener('click', () => uiManager.toggleOptionMenu())
+}
+
+const initializeLightAndDarkModeButtons = (
+  themeManager: ThemeManager,
+  lightModeButton: UiElement,
+  darkModeButton: UiElement
+) => {
+  const toggleLightAndDarkButton = (): void => {
+    lightModeButton.classList.toggle(HIDE_CLASS)
+    darkModeButton.classList.toggle(HIDE_CLASS)
   }
 
   lightModeButton.addEventListener('click', () => {
@@ -138,6 +168,41 @@ if (lightModeButton && darkModeButton && uiAppElement) {
     themeManager.activateDarkMode()
     toggleLightAndDarkButton()
   })
-} else {
-  console.warn('Light mode button or Dark Mode button not found')
 }
+
+const setInitialButtonState = (
+  themeManager: ThemeManager,
+  lightModeButton: UiElement,
+  darkModeButton: UiElement
+): void => {
+  if (themeManager.getThemeMode() === THEME_LIGHT) {
+    lightModeButton.classList.add(HIDE_CLASS)
+    darkModeButton?.classList.remove(HIDE_CLASS)
+  } else {
+    darkModeButton?.classList.add(HIDE_CLASS)
+    lightModeButton?.classList.remove(HIDE_CLASS)
+  }
+}
+
+const initializeThemeManager = (
+  uiAppElement: UiElement,
+  lightModeButton: UiElement,
+  darkModeButton: UiElement
+) => {
+  checkElement(uiAppElement, 'Ui app Element')
+  checkElement(lightModeButton, 'light mode button')
+  checkElement(darkModeButton, 'dark mode button')
+
+  const themeManager = new ThemeManager(uiAppElement)
+
+  setInitialButtonState(themeManager, lightModeButton, darkModeButton)
+
+  initializeLightAndDarkModeButtons(
+    themeManager,
+    lightModeButton,
+    darkModeButton
+  )
+}
+
+initializeThemeManager(uiAppElement, lightModeButton, darkModeButton)
+initializeUiManager(optionsMenu, optionsMenuIcon)
